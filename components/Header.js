b36352417ec09e8489f77abe86f52c1e2a2fee22
alchain <alchaincyf@gmail.com>
2024-10-26 19:39:05 +0800

    删除不必要的代码

--- components/Header.js
+++ components/Header.js
@@ -15,34 +15,27 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link href="/" className="navbar-brand">
-            CoderWithAI
-          </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <Link href="/" className={`nav-link ${router.pathname === '/' ? 'active' : ''}`}>
-                  Home
-                </Link>
-              </li>
-              {languages.map((lang) => (
-                <li key={lang.path} className="nav-item">
-                  <Link href={lang.path} className={`nav-link ${router.pathname.startsWith(lang.path) ? 'active' : ''}`}>
-                    {lang.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            <a href="https://github.com/alchaincyf/CoderWithAI" className="btn btn-outline-dark" target="_blank" rel="noopener noreferrer">
-              <i className="bi bi-github"></i> GitHub
-            </a>
-          </div>
-        </div>
+      <nav>
+        <Link href="/">
+          CoderWithAI
+        </Link>
+        <ul>
+          <li>
+            <Link href="/" className={router.pathname === '/' ? 'active' : ''}>
+              Home
+            </Link>
+          </li>
+          {languages.map((lang) => (
+            <li key={lang.path}>
+              <Link href={lang.path} className={router.pathname.startsWith(lang.path) ? 'active' : ''}>
+                {lang.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <a href="https://github.com/alchaincyf/CoderWithAI" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
       </nav>
     </header>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,6 @@
 import Layout from '../components/Layout';
-import { useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap');
-  }, []);
-
   return (
     <Layout>
       <Component {...pageProps} />
